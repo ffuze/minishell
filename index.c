0f716d83b69731efd72b2a978419e345f6cd879c
Alek <alex.deglinnocenti1@gmail.com>
2025-04-12 08:24:06 +0200

    implemented ls -l, ls, exit and 'no command found' commands, handled basic signals, started implementing clear command

--- index.c
+++ index.c
@@ -6,7 +6,7 @@
 /*   By: adegl-in <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 08:30:17 by adegl-in          #+#    #+#             */
-/*   Updated: 2025/04/11 11:58:21 by adegl-in         ###   ########.fr       */
+/*   Updated: 2025/04/11 17:13:25 by adegl-in         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,10 +51,35 @@ void	ft_ls_l()
 		wait(NULL);
 }
 
+void	ft_clear(char *input)
+{
+	char *argv[] = { "clear", NULL };
+	execve("/usr/bin/clear", argv, __environ);
+	rl_clear_history();
+	free(input);
+}
+
+void	ft_handler()
+{
+	write(1, "\n", 1);
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_redisplay();
+}
+
 int main(/*int argc, char **argv*/)
 {
+	struct sigaction sa;
 	char	*input;
 
+	sa.sa_handler = ft_handler;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGTERM, &sa, NULL);
+	sigaction(SIGSEGV, &sa, NULL);
+	sigaction(SIGTSTP, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
 	input = malloc(sizeof(char *));
 	while (1)
 	{
@@ -67,14 +92,12 @@ int main(/*int argc, char **argv*/)
 			ft_ls();
 		else if (ft_strcmp(input, "ls -l") == 0)
 			ft_ls_l();
+		else if (ft_strcmp(input, "exit") == 0)
+			return EXIT_SUCCESS;
 		else
 			printf("Command not found: %s\n", input);
 		if (ft_strcmp(input, "clear") == 0)
-		{
-			rl_clear_history();
-			// execve();
-			free(input);
-		}
+			ft_clear(input);
 		else
 			add_history(input);
 	}
--- index.h
+++ index.h
@@ -6,14 +6,23 @@
 /*   By: adegl-in <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 08:30:24 by adegl-in          #+#    #+#             */
-/*   Updated: 2025/04/11 11:50:58 by adegl-in         ###   ########.fr       */
+/*   Updated: 2025/04/11 16:32:51 by adegl-in         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./libft/libft.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/wait.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# define _DEFAULT_SOURCE
+# define _XOPEN_SOURCE 700
+
+# include "./libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+
+#endif
