8f54cc644177118ba9742637b932054a5fe7ea58
Leonardo Marino <leo-marino@hotmail.it>
2025-04-14 17:56:12 +0200

    Update index.h

--- index.h
+++ index.h
@@ -25,6 +25,41 @@
 # include <readline/history.h>
 # include <signal.h>
 
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MACROS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~COLORS~~~~~~~~~~~~~~~*/
+# define NO_ALL "\033[0m"
+
+# define NO_COLOR "\e[39m"
+# define BLACK "\e[30m"
+# define RED "\033[31m"
+# define GREEN "\e[32m"
+# define BRGREEN "\033[32;1m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[95m"
+# define BRCYAN "\033[96m"
+
+/*~~~~~~~~~~~~BACKGROUNDS~~~~~~~~~~~~~*/
+# define BGMAGENTA "\033[45m"
+# define BLACK_ON_WHITE "\e[107;30m"
+# define NO_BG "\e[49m"
+
+/*~~~~~~~~~~~~~~~MORE~~~~~~~~~~~~~~~*/
+# define BOLD "\e[1m"// "\e[1;4m" Bold and Underlined
+# define NO_BOLD "\e[21m"
+
+# define UNDERLINE "\e[4m"
+# define NO_UNDERLINE "\e[24m"
+
+# define BLINK "\e[5m"//Doesn't work on vscode terminal
+# define NO_BLINK "\e[25m"
+
+# define HIDE "\e[8m"
+# define NO_HIDE "\e[28m"
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
 void	ft_echo(char *input);
 
 #endif
