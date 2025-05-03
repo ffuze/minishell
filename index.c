c6db6cbc8bb6f78239b6f26a1331c200cb278773
Alek <alex.deglinnocenti1@gmail.com>
2025-05-03 12:13:50 +0200

    fixed almost all the cases of echo

--- index.c
+++ index.c
@@ -100,8 +100,8 @@ int main(int ac, char *av[], char **envp)
 		// 	ft_ls(split_input);
 		// else if (ft_strcmp(split_input[0], "ls -l") == 0)
 		// 	ft_ls_l();
-		// else if ((ft_strcmp(split_input[0], "echo")) == 0)
-		// 	ft_echo(split_input);
+		else if (tokens[0] && ft_strcmp(tokens[0]->value, "echo") == 0)
+			ft_echo(tokens);
 		else if (tokens[0] && tokens[0]->type == TOKEN_WORD
 			&& ft_strcmp(tokens[0]->value, "clear") == 0)
 		{
